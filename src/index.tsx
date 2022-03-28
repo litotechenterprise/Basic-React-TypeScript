a337e184bde7f6ab19768ee77a5fdd93ef766424
litotechenterprise <litotechenterprise@gmail.com>
2022-03-28 18:06:58 -0400

    added more state components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom";
 import UserSearch from "./state/UserSearch";
 
+
+
+
+
+
 const App = () => {
     return(
         <div>
--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+const users = [
+    {name:"Peter", age: 34},
+    {name:"Frank", age: 20},
+    {name:"Pablo", age: 25},
+    {name:"Sam", age: 44},
+]
 
 const UserSearch: React.FC = () => {
     return <div>User Search</div>
